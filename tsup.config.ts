a36a5cd4485fbf2f79cb4ffe05a549e554e51ffb
Ribhararnus Pracutian <oknoorap@gmail.com>
2023-06-11 17:26:08 +0700

    chore: improve compiler file size

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   terserOptions: {
+    enclose: true,
+    compress: {
+      booleans_as_integers: true,
+      unsafe: true,
+    },
     mangle: {
       properties: {
         regex:
-          /^(onInit|onMounted|onUnmounted|onAttrsUpdate|onLocalStatesUpdate|onRegistered|onPrepare|onAttributeChanged|_clear|_html|_content|_attach|_append|_render|_remove|_createTag|_attrs|_attr|_data|_datas|_reqAnimFrame|_slotTo|\$randId|\$html|\$qs|\$qsAll|\$qsString|\$children|\$attr|\$el|\$els|\$shadowEl|\$shadowEls|\$slotAttrName|\$slotStateName|setItem|getItem|subscribe)$/,
+          /^(onInit|onMounted|onUnmounted|onAttrsUpdate|onLocalStatesUpdate|onRegistered|onPrepare|onAttributeChanged|_clear|_html|_content|_attach|_append|_render|_remove|_createTag|_attrs|_attr|_data|_datas|_reqAnimFrame|_slotTo|\$randId|\$html|\$qs|\$qsAll|\$qsString|\$children|\$attr|\$el|\$els|\$shadowEl|\$shadowEls|\$slotAttrName|\$slotStateName|setItem|getItem|subscribe|TRIGGER)$/,
       },
     },
   },
