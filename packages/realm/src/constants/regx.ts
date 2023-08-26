b4ffb8963da600b106d6c05e3e6cebe6f559925f
Ribhararnus Pracutian <oknoorap@gmail.com>
2023-08-26 21:49:55 +0700

    fix: update css selector regex
    * now only supports alphanumeric, and special chars dot-dash-underline

--- packages/realm/src/constants/regx.ts
+++ packages/realm/src/constants/regx.ts
@@ -3,6 +3,6 @@ export const UPPERCASE_ALPHABET_REGEX = /([A-Z])+/g;
 export const SPECIAL_CHARS_REGEX = /(?=[A-Z])|[.\-\s_]/;
 
 export const CSS_CUSTOM_SELECTOR_REGEX =
-  /:host\(\[\\([#@]\w+?)\](\[(global)\])?(\[(eq|neq|gt|gte|lt|lte|contains)="(\w+?)"\])?\)/g;
+  /:host\(\[\\([#@][a-zA-Z0-9-_.]+?)\](\[(global)\])?(\[(eq|neq|gt|gte|lt|lte|contains)="(\w+?)"\])?\)/g;
 
 export const DOT_NOTATION_REGEX = /[$|#|@|!]([.]?(?:\w+[.-])*\w+)/gm;
