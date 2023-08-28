cf9777431237cce90870124fd65904dac9509ad8
Ribhararnus Pracutian <oknoorap@gmail.com>
2023-08-28 23:36:22 +0700

    fix: differentiate regex between safari & others

--- packages/realm/src/constants/regx.ts
+++ packages/realm/src/constants/regx.ts
@@ -2,7 +2,10 @@ export const UPPERCASE_ALPHABET_REGEX = /([A-Z])+/g;
 
 export const SPECIAL_CHARS_REGEX = /(?=[A-Z])|[.\-\s_]/;
 
-export const CSS_CUSTOM_SELECTOR_REGEX =
-  /:host\(\[\\([#@][a-zA-Z0-9-_.]+?)\](\[(global)\])?(\[(eq|neq|gt|gte|lt|lte|contains)="(\w+?)"\])?\)/g;
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+export const CSS_CUSTOM_SELECTOR_REGEX = isSafari
+  ? /:host\(\[([#@][a-zA-Z0-9-_.]+?)\](\[(global)\])?(\[(eq|neq|gt|gte|lt|lte|contains)=\"(\w+?)\"\])?\)/g
+  : /:host\(\[\\([#@][a-zA-Z0-9-_.]+?)\](\[(global)\])?(\[(eq|neq|gt|gte|lt|lte|contains)="(\w+?)"\])?\)/g;
 
 export const DOT_NOTATION_REGEX = /[$|#|@|!]([.]?(?:\w+[.-])*\w+)/gm;
