56c7ce7933599338e3e6b05fa65bf98adb2045c8
Ribhararnus Pracutian <oknoorap@gmail.com>
2023-08-31 11:45:14 +0700

    chore: removed `astro` compress

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
 import critters from "astro-critters";
 
 // https://astro.build/config
@@ -16,11 +15,6 @@ export default defineConfig({
       selector: ["anchor-link[href]", "anchor-gradient[href]"],
     }),
     sitemap(),
-    compress({
-      html: false,
-      css: true,
-      js: true,
-    }),
     critters({
       path: ["./dist"],
     }),
