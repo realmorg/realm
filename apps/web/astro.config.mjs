2de91357c1f7b6aadcff8f3b58e243fc24d594eb
Ribhararnus Pracutian <oknoorap@gmail.com>
2023-08-31 11:36:32 +0700

    Fix several errors when build (#26)
    * fix: added space unicode caused by minifier
    
    * feat: added site final url

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -8,6 +8,7 @@ import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://realm.codes",
   integrations: [
     mdx(),
     partytown(),
